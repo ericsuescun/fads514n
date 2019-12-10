17e023573c9cddc14a13c787d5c8195ad241e1ea
Eric Suescun <edsuescun@gmail.com>
2019-12-10 15:23:56 -0500

    Se implementa el barrido por el list.!

--- src/App.js
+++ src/App.js
@@ -15,6 +15,9 @@ class App extends Component {
         <h1 style={styles.title}>Las tecnologias del Front</h1>
         <ul style={styles.list}>
           {/* Tu codigo va aca */}
+          {this.state.list.map((element) => 
+            <li key={element}>{element}</li>
+          )}
         </ul>
       </div>
     );
